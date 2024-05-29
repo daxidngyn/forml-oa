5a0b687667ee85172f37257d3504878b95005da3
daxidngyn <daxidnguyen@gmail.com>
2024-05-28 18:06:48 -0700

    [feat] implement frontend logic

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
+  const submitCombination = async (formData: FormData) => {
+    "use server";
+
+    const rawFormData = {
+      actual_combination: formData.get("combination"),
+    };
+
+    const res = await fetch("http://127.0.0.1:5000/api/crack_safe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(rawFormData),
+    });
+
+    const data = await res.json();
+    const attempts = data.attempts;
+    const time_taken = data.time_taken;
+
+    console.log(attempts, time_taken);
+  };
+
   return (
     <main className="">
       <h1>Forml OA</h1>
+      <div>
+        <form action={submitCombination}>
+          <input name="combination" placeholder="Combination" />
+          <button>Submit</button>
+        </form>
+      </div>
     </main>
   );
 }
